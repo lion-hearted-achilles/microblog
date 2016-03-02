a4690e961394fe734adb9391d79b66f7ce07b443
lion-hearted-achilles <geoff.roeder@gmail.com>
2016-03-02 11:13:46 -0800

    modify init to add debug and error reporting

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 import os
+
+
+
 # must define lm here to avoid circular references
 # https://flask.readthedocs.org/en/latest/patterns/packages/
 app = Flask(__name__)
@@ -15,6 +18,13 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 app.config.from_object('config')
 # initialize database when we intialize the app
 db = SQLAlchemy(app)
+if not app.debug:
+   import logging
+   from logging.andlers import SMPTHandler
+   credentials = None
+   if MAIL_USERNAME or MAIL_PASSWORD:
+      credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+
 # avoid circular references here
 from app import views, models
 
